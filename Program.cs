0d695d1614336ea71fc1ab0d67787d65eaca4aad
Yt1m <144239923+Yt1m@users.noreply.github.com>
2023-11-11 17:20:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите строку");
+char[] mas = Console.ReadLine().ToCharArray();
+        int b = mas.Length / 2;
+        for (int i = 0; i < b; i++)
+        {
+            if (mas[i] == ':')
+            {
+                mas[i] = '.';
+            }
+        }
+        for (int i = b; i < mas.Length; i++)
+        {
+            if (mas[i] == '!')
+            {
+                mas[i] = '.';
+            }
+        }
+        Console.WriteLine(new string(mas));
