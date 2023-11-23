586afd6f494268337cc5ee66a75c5700a67fb6b1
Yt1m <144239923+Yt1m@users.noreply.github.com>
2023-11-23 12:52:19 +0200

    Add files via upload

---
+++ 5-2.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string input = "*****";
+        string output = ReplaceStars(input);
+
+        Console.WriteLine(output);
+    }
+    static string ReplaceStars(string input)
+    {
+        Dictionary<char, int> starCounter = new Dictionary<char, int>();
+        string output = "";
+        foreach (char c in input)
+        {
+            if (c == '*')
+            {
+                if (starCounter.ContainsKey(c))
+                {
+                    starCounter[c]++;
+                }
+                else
+                {
+                    starCounter[c] = 1;
+                }
+
+                output += starCounter[c].ToString();
+            }
+            else
+            {
+                output += c;
+            }
+        }
+        return output;
+    }
+}
